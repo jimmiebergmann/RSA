c8c792b4425c843bce3ea2b16284d21745061127
jimmiebergmann <jimmiebergmann@gmail.com>
2013-04-12 16:12:46 +0200

    Added a class for large integers.

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 RSA
-===
+=====
 
 RSA encryption and decryption in c++
+
+Resources
+=====
+http://www.efgh.com/software/rsa.htm
+http://www.allaboutcircuits.com/vol_4/chpt_2/5.html
---
+++ source/LargeInteger.cpp
@@ -0,0 +1,156 @@
+///////////////////////////////////////////////////////////////////////////////////
+// Copyright (C) 2013 Jimmie Bergmann - jimmiebergmann@gmail.com
+//
+// This software is provided 'as-is', without any express or
+// implied warranty. In no event will the authors be held
+// liable for any damages arising from the use of this software.
+// 
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute
+// it freely, subject to the following restrictions:
+// 
+// 1. The origin of this software must not be misrepresented;
+//    you must not claim that you wrote the original software.
+//    If you use this software in a product, an acknowledgment
+//    in the product documentation would be appreciated but
+//    is not required.
+// 
+// 2. Altered source versions must be plainly marked as such,
+//    and must not be misrepresented as being the original software.
+// 
+// 3. This notice may not be removed or altered from any
+//    source distribution.
+// 
+///////////////////////////////////////////////////////////////////////////////////
+
+#include "LargeInteger.hpp"
+#include <algorithm>
+#include <stdlib.h>
+
+// Constructors / descructors
+LargeInteger::LargeInteger( ) :
+	m_pValue( NULL ),
+	m_Size( 0 )
+{
+}
+
+LargeInteger::LargeInteger( const unsigned int p_Size ) :
+	m_pValue( NULL ),
+	m_Size( 0 )
+
+{
+	if( Allocate( p_Size ) )
+	{
+		return;
+	}
+}
+
+LargeInteger::LargeInteger( const unsigned int p_Size, unsigned short p_Values, ... ) :
+	m_pValue( NULL ),
+	m_Size( 0 )
+{
+	if( !Allocate( p_Size ) )
+	{
+		return;
+	}
+
+	// Get the pointer to the value param in order to get all the values that we want to set.
+	unsigned short * pointer = &p_Values;
+
+	// Set every single value
+	for( unsigned int i = 0; i < p_Size; i++ )
+	{
+		// Set the current value to the value that the pointer currently is pointing at.
+		m_pValue[ i ] = *pointer;
+
+		// Move the pointer 2 bytes since we are using shorts.
+		pointer += 2;
+	}
+}
+
+LargeInteger::LargeInteger( const LargeInteger & p_LargeInteger ) :
+	m_pValue( NULL ),
+	m_Size( 0 )
+{
+	if( Allocate( p_LargeInteger.m_Size ) )
+	{
+		return;
+	}
+
+	Copy( p_LargeInteger );
+}
+
+LargeInteger::~LargeInteger( )
+{
+	// Delete the allocated values
+	if( m_pValue )
+	{
+		delete [ ] m_pValue;
+		m_pValue = NULL;
+	}
+
+	// Set the size of the large interger to 0
+	m_Size = 0;
+}
+
+// Public functions
+void LargeInteger::PrintBinary( ) const
+{
+	for( int i = m_Size - 1; i >= 0; i-- )
+	{
+		for( int j = (sizeof( unsigned short ) * 8) - 1; j >= 0; j-- )
+		{
+			std::cout << CheckBit( m_pValue[ i ], j );
+		}
+		std::cout << " ";
+	}
+}
+
+// Set functions
+void LargeInteger::SetElement( const unsigned int p_Index, const unsigned short p_Value )
+{
+	if( p_Index < m_Size )
+	{
+		m_pValue[ p_Index ] = p_Value;
+	}
+}
+
+// Get functions
+unsigned short LargeInteger::GetElement( const unsigned int p_Index ) const
+{
+	if( p_Index >= m_Size )
+	{
+		return 0;
+	}
+
+	return m_pValue[ p_Index ];
+}
+
+// Private functions
+bool LargeInteger::Allocate( const unsigned int p_Size )
+{
+	if( p_Size == 0 )
+	{
+		return false;
+	}
+
+	// Make sure to delete the old allocated data if there are any.
+	if( m_pValue )
+	{
+		delete [ ] m_pValue;
+	}
+	
+	m_Size = p_Size;
+	m_pValue = new unsigned short[ p_Size ];
+
+	return true;
+}
+
+void LargeInteger::Copy( const LargeInteger & p_LargeInteger )
+{
+	// You might lose data if the parameter large integer is larger in allocation size.
+	for( unsigned int i = 0; i < m_Size; i++ )
+	{
+		m_pValue[ i ] = p_LargeInteger.m_pValue[ i ];
+	}
+}
